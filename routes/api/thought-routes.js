24f58dad8d477b89eb42ffb0afba086e28d8216b
paul88vera <paul88vera@gmail.com>
2022-03-24 10:06:11 -0500

    routes completed

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
 const {
-
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  addReaction,
+  removeReaction
 } = require('../../controllers/thought-controller');
 
-router.route('/:userId').post(addThought);
+router.route('/').get(getAllThoughts);
 
-router.route('/:userId/:thoughtId').put(addReaction).delete(removeThought);
+router.route('/:userId').get(getThoughtById);
+
+router.route('/:userId').post(createThought);
+
+router.route('/:userId/:thoughtId').put(addReaction).delete(removeReaction);
 
 router.route(':/userId/:thoughtId/:reactionId').delete(removeReaction);
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,15 +1,21 @@
 const router = require('express').Router();
-const {} = require('../../controllers/user-controller');
+const {
+  getAllUser,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser
+} = require('../../controllers/user-controller');
 
 router
   .route('/')
-  .get()
-  .post();
+  .get(getAllUser)
+  .post(createUser);
 
 router
-  .route()
-  .get()
-  .put()
-  .delete()
+  .route('/:id')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser)
 
   module.exports = router;
